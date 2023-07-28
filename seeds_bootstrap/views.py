01b031381c15e422bfb674c7b4c0e0b8dc8bda10
Pankaj Chejara <pankajchejara23@gmail.com>
2023-07-28 19:22:08 +0300

    Update views.py
    updating results based on multiple parameters selected by the user.

--- seeds_bootstrap/views.py
+++ seeds_bootstrap/views.py
@@ -7,11 +7,19 @@ from django.shortcuts import render, redirect
 from django.utils import translation
 from django.shortcuts import redirect
 from django.core.paginator import Paginator
+from register import forms as reg_forms
+from .models import Scenario, TechGeneration
+import pandas as pd
 
-from .models import Scenario
 
 def index(request):
-    return render(request,'index.html',{})
+    form = reg_forms.LoginForm()
+    return render(request,'index.html',{'form':form})
+
+
+def rescale(value,min,max):
+    range = max - min
+    return min + value * range
 
 def selection(request):
     POWER_MIN = 13.24
@@ -20,19 +28,28 @@ def selection(request):
     STORAGE_MIN = 5.7
     STORAGE_MAX = 6.3
 
+    IMPLEMENT_MIN = 3.7
+    IMPLEMENT_MAX = 13
+
+    IMPORT_MIN = 0
+    IMPORT_MAX = .15
+
+    COMMUNITY_MIN = .18
+    COMMUNITY_MAX = 1
+
     if request.method == 'POST':
         #fetching energy systems params
-        #print('value:{} type:{}'.format(request.POST['power_0'],type(request.POST['power_0'])))
-        power_min = POWER_MIN * float(request.POST['power_0'])
-        power_max = POWER_MAX * float(request.POST['power_1'])
-        storage_min = STORAGE_MIN * float(request.POST['storage_0'])
-        storage_max = STORAGE_MAX * float(request.POST['storage_1'])
-        community_min = request.POST['community_0']
-        community_max = request.POST['community_1']
-        implementation_min = float(request.POST['implement_0'])
-        implementation_max = float(request.POST['implement_1'])
-        import_min = request.POST['import_0']
-        import_max = request.POST['import_1']
+        
+        power_min = rescale(float(request.POST['power_0']),POWER_MIN,POWER_MAX)
+        power_max = rescale(float(request.POST['power_1']),POWER_MIN,POWER_MAX)
+        storage_min = rescale(float(request.POST['storage_0']),STORAGE_MIN,STORAGE_MAX)
+        storage_max = rescale(float(request.POST['storage_1']),STORAGE_MIN,STORAGE_MAX)
+        community_min = rescale(float(request.POST['community_0']),COMMUNITY_MIN,COMMUNITY_MAX)
+        community_max = rescale(float(request.POST['community_1']),COMMUNITY_MIN,COMMUNITY_MAX)
+        implementation_min = rescale(float(request.POST['implement_0']),IMPLEMENT_MIN,IMPLEMENT_MAX)
+        implementation_max = rescale(float(request.POST['implement_1']),IMPLEMENT_MIN,IMPLEMENT_MAX)
+        import_min = rescale(float(request.POST['import_0']),IMPORT_MIN,IMPORT_MAX)
+        import_max = rescale(float(request.POST['import_1']),IMPORT_MIN,IMPORT_MAX)
 
         #fetching impact controls params
         land_min = request.POST['land_0']
@@ -71,30 +88,25 @@ def selection(request):
         battery_min = request.POST['battery_0']
         battery_max = request.POST['battery_1']
         
-        print('Power min:{} max:{}'.format(power_min,power_max))
-
-        print('Hyodro-river min:{} max:{}'.format(hydro_river_min,hydro_river_max))
-        print('wind-on-shore min:{} max:{}'.format(wind_on_shore_min,wind_on_shore_max))
-        print('Human min:{} max:{}'.format(human_min,human_max))
+        #print('Power min:{} max:{}'.format(power_min,power_max))
+        #print('Hyodro-river min:{} max:{}'.format(hydro_river_min,hydro_river_max))
+        #print('wind-on-shore min:{} max:{}'.format(wind_on_shore_min,wind_on_shore_max))
+        #print('Human min:{} max:{}'.format(human_min,human_max))
 
         # Scale of parameters in database
         scenarios_community = Scenario.objects.all().filter(community_infrastructure__range=(community_min,community_max))
         scenarios_power = scenarios_community.filter(power_capacity__range=(power_min,power_max))
         scenarios_storage = scenarios_power.filter(storage_capacity__range=(storage_min,storage_max))
-        scenarios_implementation = scenarios_power.filter(implementation_pace__range=(implementation_min,implementation_max))
-        scenarios_import = scenarios_implementation.filter(import_dependency__range=(import_min,import_max))
-
-
-        techs_gen = TechGeneration.objects()
-
-        #scenarios_implementation = scenarios_storage
-
-        paginator = Paginator(scenarios_import, 10, orphans=3)
-        total_obs = len(scenarios_import)
+        scenarios_implementation = scenarios_storage.filter(implementation_pace__range=(implementation_min,implementation_max))
+        scenarios_import = scenarios_implementation.filter(import_dependency__range=(import_min,import_max)).order_by('id')
         
+ 
+        #scenarios_implementation = scenarios_storage
+        paginator = Paginator(scenarios_import, 20, orphans=3)
+        total_obs = len(scenarios_import)
         page_obj = paginator.get_page(1)
-
         page_range = list(paginator.get_elided_page_range(1))
+        print('Tota objects:',len(scenarios_import))
         return render(request,'show_results.html',{'page_obj':page_obj,'page_range':page_range})
 
     else:
