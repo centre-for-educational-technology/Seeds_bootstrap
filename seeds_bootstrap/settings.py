962962153ff4441dd413c976351893c440d1edb1
Pankaj Chejara <pankajchejara23@gmail.com>
2023-09-21 16:59:48 +0300

    changing host

--- seeds_bootstrap/settings.py
+++ seeds_bootstrap/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6o8s_(%8iu(5ngpz$!e8tif4f!mp)jtk#%j$=gea&1vme9u4ni
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','86.107.197.103']
 LOGIN_URL = 'login'
 
 # Application definition
