a0d71d5f223869546089f067ed516ba19436a1ae
Pankaj Chejara <pankajchejara23@gmail.com>
2023-12-19 14:25:21 +0200

    update user creation script

--- scripts/create_users.py
+++ scripts/create_users.py
@@ -15,14 +15,15 @@ def run():
     for i in range(50):
         user_name = 'user' + str(i)
         user_email = 'user' + str(i) + '@seeds.org'
-        user_pwd = make_password(randomword(5))
+        temp_pwd = randomword(5)
+        user_pwd = make_password(temp_word)
         first_name = user_name
         last_name = 'seeds'
         try:
             print('creating user-',i)
             obj = User.objects.create(first_name=first_name,last_name=last_name,username=user_name,email=user_email,password=user_pwd)
             ids.append(user_email)
-            pwds.append(user_pwd)
+            pwds.append(temp_pwd)
         except:
             print('error occurred.')
 
