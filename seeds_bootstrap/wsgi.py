74e8d932b3b3a654384b3349e07a0d22058ec596
Pankaj Chejara <pankajchejara23@gmail.com>
2024-02-15 17:26:03 +0200

    update wsgi

--- seeds_bootstrap/wsgi.py
+++ seeds_bootstrap/wsgi.py
@@ -10,6 +10,11 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import sys
+
+sys.path.append('/home/Seeds_bootstrap')
+sys.path.append('/home/Seeds_bootstrap/seeds_bootstrap')
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seeds_bootstrap.settings')
 
